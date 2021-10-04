2b09f4a51b89bd754b41eb6319121385b4c2cad0
Eduard-Developer <91020951+Eduard-Developer@users.noreply.github.com>
2021-10-04 16:48:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const navSlide = () =>{
+  const nav = document.querySelector(".nav-links");
+   const burger = document.querySelector(".burger");
+   const navlinks = document.querySelectorAll(".nav-links li") 
+     
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navlinks.forEach((link, index) => { 
+        if (link.style.animation){
+            link.style.animation = "";
+        }else{
+            link.style.animation = `Navlinkop 0.5s ease forwards ${index / 5 + 0.75}s`;
+        }
+
+    });
+    });
+}
+
+navSlide();
