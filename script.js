a5543e914259c645310f83c9035e0bff6d1a8e5a
Eduard Berisha <91020951+Eduard-Developer@users.noreply.github.com>
2022-08-21 16:45:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const navSlide = () =>{
-  const nav = document.querySelector(".nav-links");
+/*const navSlide = () =>{
+   const nav = document.querySelector(".nav-links");
    const burger = document.querySelector(".burger");
    const navlinks = document.querySelectorAll(".nav-links li") 
    const title = document.querySelector(".Title h1")
      
 
 burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    navlinks.forEach((link, index) => { 
-        if (link.style.animation){
-            link.style.animation = "";
-        }else{
-            link.style.animation = `Navlinkop 0.5s ease forwards ${index / 5 + 0.75}s`;
-        }
+    //nav.classList.toggle("nav-active");
+    //navlinks.forEach((link, index) => { 
+        //if (link.style.animation){
+            //link.style.animation = "";
+        //}else{
+            //link.style.animation = `Navlinkop 0.5s ease forwards ${index / 5 + 0.75}s`;
+        //}
 
-    });
+    //});
+
+    window.alert("Work in progress!")
     });
 }
 navSlide();
@@ -23,4 +25,17 @@ navSlide();
 
 window.alert("Willkommen!");
 
-//}
+//}*/
+const _kontakt = () => {
+    const _kontaktformular = document.getElementsByClassName("kontakt");
+}
+
+
+_kontaktformular.addEventListener("click", () => {
+
+    window.alert("Work in progress!");
+
+
+})
+
+_kontakt(); 
