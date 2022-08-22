93cb2afb2b04dc7e5c97868634f8d5a44eed9475
Eduard Berisha <91020951+Eduard-Developer@users.noreply.github.com>
2022-08-22 21:14:27 +0200

    Launching this baby

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const kontakt = document.getElementById('kontakt');
+function onClick(){      
+	
+
+    console.log("it is finally ok and the window.alert() was the actual fault");
+}
+if(kontakt){
+    kontakt.addEventListener("click", onClick(), false);
+}
+
 /*const navSlide = () =>{
    const nav = document.querySelector(".nav-links");
    const burger = document.querySelector(".burger");
@@ -17,25 +27,27 @@ burger.addEventListener("click", () => {
     //});
 
     window.alert("Work in progress!")
-    });
-}
-navSlide();
+    });}*/
 
-//function windowalert(){
 
-window.alert("Willkommen!");
 
-//}*/
-const _kontakt = () => {
-    const _kontaktformular = document.getElementsByClassName("kontakt");
-}
+
+
+
+
+/*const _kontaktformular = document.getElementsByClassName("kontakt");
+
 
 
 _kontaktformular.addEventListener("click", () => {
-
+    console.log("Work in progress!")
     window.alert("Work in progress!");
 
 
-})
+})*/
 
-_kontakt(); 
+
+//_kontaktformular();
+
+
+ 
